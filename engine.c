b7a92c9eac8a2f70ed3fbec893fa7e064fd91950
Kevin <1328037882@qq.com>
2025-04-30 16:04:32 +0800

    update

--- engine.c
+++ engine.c
@@ -1,35 +1,31 @@
-// NodeMCU Based WIFI Controlled Car//
-
-#define ENA   14          // Enable/speed motors Right        GPIO14(D5)
-#define ENB   12          // Enable/speed motors Left         GPIO12(D6)
-#define IN_1  15          // L298N in1 motors Right           GPIO15(D8)
-#define IN_2  13          // L298N in2 motors Right           GPIO13(D7)
-#define IN_3  2           // L298N in3 motors Left            GPIO2(D4)
-#define IN_4  0           // L298N in4 motors Left            GPIO0(D3)
-
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h> 
 #include <ESP8266WebServer.h>
 
-String command;             //String to store app command state.
-int speedCar = 800;         // 400 - 1023.
-int speed_Coeff = 3;
-
-const char* ssid = "NodeMCU Car";
-ESP8266WebServer server(80);
+// 定义 L9110S 电机驱动模块的输入引脚
+const int MotorA1 = D1;
+const int MotorA2 = D2;
+const int MotorB1 = D3;
+const int MotorB2 = D4;
+const int MotorC1 = D5;
+const int MotorC2 = D6;
+const int MotorD1 = D7;
+const int MotorD2 = D8;
 
 void setup() {
- 
- pinMode(ENA, OUTPUT);
- pinMode(ENB, OUTPUT);  
- pinMode(IN_1, OUTPUT);
- pinMode(IN_2, OUTPUT);
- pinMode(IN_3, OUTPUT);
- pinMode(IN_4, OUTPUT); 
-  
+  // 设置引脚为输出模式
+  pinMode(MotorA1, OUTPUT);
+  pinMode(MotorA2, OUTPUT);
+  pinMode(MotorB1, OUTPUT);
+  pinMode(MotorB2, OUTPUT);
+  pinMode(MotorC1, OUTPUT);
+  pinMode(MotorC2, OUTPUT);
+  pinMode(MotorD1, OUTPUT);
+  pinMode(MotorD2, OUTPUT);
+
   Serial.begin(115200);
- 
-// Connecting WiFi
+
+  // Connecting WiFi
 
   WiFi.mode(WIFI_AP);
   WiFi.softAP(ssid);
@@ -43,110 +39,44 @@ void setup() {
      server.begin();    
 }
 
-void goAhead(){ 
+void goforwards(){ 
 
-      digitalWrite(IN_1, LOW);
-      digitalWrite(IN_2, HIGH);
-      analogWrite(ENA, speedCar);
+    analogWrite(MotorA1,speed);
+    analogWrite(MotorA2,speed);
+    analogWrite(MotorB1,speed);
+    analogWrite(MotorB2,speed);
+    analogWrite(MotorC1,speed);
+    analogWrite(MotorC2,speed);
+    analogWrite(MotorD1,speed);
+    analogWrite(MotorD2,speed);
 
-      digitalWrite(IN_3, LOW);
-      digitalWrite(IN_4, HIGH);
-      analogWrite(ENB, speedCar);
-  }
+}
 
-void goBack(){ 
+void stop(){ 
 
-      digitalWrite(IN_1, HIGH);
-      digitalWrite(IN_2, LOW);
-      analogWrite(ENA, speedCar);
+    digitalWrite(MotorA1, LOW);
+    digitalWrite(MotorA2, LOW);
+    digitalWrite(MotorB1, LOW);
+    digitalWrite(MotorB2, LOW);
+    digitalWrite(MotorC1, LOW);
+    digitalWrite(MotorC2, LOW);
+    digitalWrite(MotorD1, LOW);
+    digitalWrite(MotorD2, LOW);
 
-      digitalWrite(IN_3, HIGH);
-      digitalWrite(IN_4, LOW);
-      analogWrite(ENB, speedCar);
-  }
+}
 
-void goRight(){ 
+String command;             //String to store app command state.
+int speed = 800;         // 400 - 1023.
+int speed_Coeff = 3;
 
-      digitalWrite(IN_1, HIGH);
-      digitalWrite(IN_2, LOW);
-      analogWrite(ENA, speedCar);
-
-      digitalWrite(IN_3, LOW);
-      digitalWrite(IN_4, HIGH);
-      analogWrite(ENB, speedCar);
-  }
-
-void goLeft(){
-
-      digitalWrite(IN_1, LOW);
-      digitalWrite(IN_2, HIGH);
-      analogWrite(ENA, speedCar);
-
-      digitalWrite(IN_3, HIGH);
-      digitalWrite(IN_4, LOW);
-      analogWrite(ENB, speedCar);
-  }
-
-void goAheadRight(){
-      
-      digitalWrite(IN_1, HIGH);
-      digitalWrite(IN_2, LOW);
-      analogWrite(ENA, speedCar/speed_Coeff);
- 
-      digitalWrite(IN_3, LOW);
-      digitalWrite(IN_4, HIGH);
-      analogWrite(ENB, speedCar);
-   }
-
-void goAheadLeft(){
-      
-      digitalWrite(IN_1, LOW);
-      digitalWrite(IN_2, HIGH);
-      analogWrite(ENA, speedCar);
-
-      digitalWrite(IN_3, HIGH);
-      digitalWrite(IN_4, LOW);
-      analogWrite(ENB, speedCar/speed_Coeff);
-  }
-
-void goBackRight(){ 
-
-      digitalWrite(IN_1, LOW);
-      digitalWrite(IN_2, HIGH);
-      analogWrite(ENA, speedCar/speed_Coeff);
-
-      digitalWrite(IN_3, HIGH);
-      digitalWrite(IN_4, LOW);
-      analogWrite(ENB, speedCar);
-  }
-
-void goBackLeft(){ 
-
-      digitalWrite(IN_1, HIGH);
-      digitalWrite(IN_2, LOW);
-      analogWrite(ENA, speedCar);
-
-      digitalWrite(IN_3, LOW);
-      digitalWrite(IN_4, HIGH);
-      analogWrite(ENB, speedCar/speed_Coeff);
-  }
-
-void stopRobot(){  
-
-      digitalWrite(IN_1, LOW);
-      digitalWrite(IN_2, LOW);
-      analogWrite(ENA, speedCar);
-
-      digitalWrite(IN_3, LOW);
-      digitalWrite(IN_4, LOW);
-      analogWrite(ENB, speedCar);
- }
+const char* ssid = "NodeMCU Car";
+ESP8266WebServer server(80);
 
 void loop() {
     server.handleClient();
     
       command = server.arg("State");
-      if (command == "F") goAhead();
+      if (command == "F") goforwards();
       else if (command == "B") goBack();
       else if (command == "L") goLeft();
       else if (command == "R") goRight();
@@ -154,24 +84,15 @@ void loop() {
       else if (command == "G") goAheadLeft();
       else if (command == "J") goBackRight();
       else if (command == "H") goBackLeft();
-      else if (command == "0") speedCar = 400;
-      else if (command == "1") speedCar = 470;
-      else if (command == "2") speedCar = 540;
-      else if (command == "3") speedCar = 610;
-      else if (command == "4") speedCar = 680;
-      else if (command == "5") speedCar = 750;
-      else if (command == "6") speedCar = 820;
-      else if (command == "7") speedCar = 890;
-      else if (command == "8") speedCar = 960;
-      else if (command == "9") speedCar = 1023;
-      else if (command == "S") stopRobot();
-}
-// 网络协议 直接抄？
-void HTTP_handleRoot(void) {
-
-if( server.hasArg("State") ){
-       Serial.println(server.arg("State"));
-  }
-  server.send ( 200, "text/html", "" );
-  delay(1);
+      else if (command == "0") speed = 400;
+      else if (command == "1") speed = 470;
+      else if (command == "2") speed = 540;
+      else if (command == "3") speed = 610;
+      else if (command == "4") speed = 680;
+      else if (command == "5") speed = 750;
+      else if (command == "6") speed = 820;
+      else if (command == "7") speed = 890;
+      else if (command == "8") speed = 960;
+      else if (command == "9") speed = 1023;
+      else if (command == "S") stop();
 }
